ae34b56f9e318d492ce0dd44300534c94f19776e
Nitsou <45285879+Nitsou@users.noreply.github.com>
2020-02-04 12:42:01 +0100

    Create Book-list-database.sql

---
+++ SQL/Book-list-database.sql
@@ -0,0 +1,8 @@
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+INSERT INTO books VALUES (1, "The Witcher", 4);
+INSERT INTO books VALUES (2, "Harry Potter", 4.5);
+INSERT INTO books VALUES (3, "Journal d'un dégonflé", 4.7);
+SELECT * FROM books;
+SELECT * FROM books;
+SELECT * FROM books;
+SELECT * FROM books;
